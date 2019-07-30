049e3796b0a609e51532c798a1bfd03985b68610
Given Ndou <ndougiven3@gmail.com>
2019-07-30 15:07:24 +0200

    I have added  the template.py file

--- Prac1/template.py
+++ Prac1/template.py
@@ -3,25 +3,27 @@
 Python Practical Template
 Keegan Crankshaw
 Readjust this Docstring as follows:
-Names: <names>
-Student Number: <studnum>
-Prac: <Prac Num>
-Date: <dd/mm/yyyy>
+Names: Given Ndou
+Student Number: ndxgiv001
+Prac: 1
+Date: 28/07/2019
 """
 
 # import Relevant Librares
 import RPi.GPIO as GPIO
 
-# Logic that you write
+
+
+
 def main():
-    print("write your logic here")
+    
 
 
 # Only run the functions if 
 if __name__ == "__main__":
     # Make sure the GPIO is stopped correctly
     try:
-        while True:
+        #while True:
             main()
     except KeyboardInterrupt:
         print("Exiting gracefully")
