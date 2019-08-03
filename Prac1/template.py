ee619ce3bd7dcaa4a902903f4b7b00a357d6eb05
Given Ndou <ndougiven3@gmail.com>
2019-08-03 22:03:34 +0200

    I have added the comments to my code

--- Prac1/template.py
+++ Prac1/template.py
@@ -12,7 +12,7 @@ Date: 28/07/2019
 # import Relevant Librares
 import RPi.GPIO as GPIO
 myList = [0]
-
+#increment if the operand is plus, else decrement
 def operator(oprtr):
     if(oprtr=="plus"):
         myList[0]=(myList[0]+1)%8
@@ -21,10 +21,12 @@ def operator(oprtr):
             myList[0] = 7
         else:
             myList[0]=myList[0]-1
-
+#this method is for blinking the leds
 def blinkLeds():
-    bitss = bin(myList[0]).zfill(4)
+    bitss = bin(myList[0]).zfill(4) # get 3 bit for every value
     bits=bitss.replace('b', '')
+	
+	#access all the bits generated
     firstBit =int(bits[-1])
     secBit=int(bits[-2])
     thirdBit=int(bits[-3])
@@ -33,16 +35,20 @@ def blinkLeds():
     GPIO.output(8,firstBit)
     GPIO.output(10,secBit)
     GPIO.output(12,thirdBit)
-
+#this method is a call back for decrementing
 def button_callback2(channel):
     
     operator("minus")
     blinkLeds()	
+#this method is a call back for incrementing
 def button_callback(channel):
     blinkLeds()
     operator("plus")
+#main method where execution starts
 def main():
 GPIO.setwarnings(False)
+
+	#setup the pins to output/input
     GPIO.setmode(GPIO.BOARD)
     GPIO.setup(8,GPIO.OUT,initial=GPIO.LOW)
     GPIO.setup(12,GPIO.OUT,initial=GPIO.LOW)
@@ -50,6 +56,8 @@ GPIO.setwarnings(False)
     GPIO.setup(16, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
     GPIO.setup(18, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
     #GPIO.add_event_detect(16,GPIO.RISING,callback=button_callback)
+	
+	#set interrupts to detect when a button is clicked
     GPIO.add_event_detect(16, GPIO.FALLING, callback=button_callback,bouncetime=300)
     
     GPIO.add_event_detect(18,GPIO.RISING,callback=button_callback2,bouncetime=300)
